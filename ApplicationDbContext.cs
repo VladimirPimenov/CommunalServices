3c314b4fbe94e548b6f28456675355875347702e
Vladimir <pimenovv2000@gmail.com>
2025-03-12 20:32:09 +0300

    Перемещён файл контекста БД

--- ApplicationDbContext.cs
+++ ApplicationDbContext.cs
@@ -1,13 +1,13 @@
 ﻿using CommunalServices.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace CommunalServices.Data
+namespace CommunalServices
 {
     public class ApplicationDbContext : DbContext
     {
         public DbSet<Abonent> Abonent { get; set; }
         public DbSet<Flat> Flat { get; set; }
         public DbSet<Debt> Debt { get; set; }
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }		
-	}
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    }
 }
