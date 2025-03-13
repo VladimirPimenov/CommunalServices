7beac9f6c808e2ef9ea106bc8c3c7b317b27a944
Vladimir <pimenovv2000@gmail.com>
2025-03-14 00:52:47 +0300

    Создан контроллер для модели PaymentAccount

---
+++ Controllers/PaymentAccount.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CommunalServices.Controllers
+{
+    [Route("/PaymentAccount")]
+    [ApiController]
+    public class PaymentAccount(ApplicationDbContext dbContext) : Controller
+    {
+        [HttpGet("{paymentId}")]
+        public IActionResult GetPaymentAccountById(Guid paymentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("{paymentId}")]
+        public IActionResult RemovePaymentAccountById(Guid paymentId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
