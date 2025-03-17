7938d65c2210b4cafa7b78fadf5f6d23810734d8
Vladimir <pimenovv2000@gmail.com>
2025-03-18 00:34:53 +0300

    Подключен PaymentAccount

--- ApplicationDbContext.cs
+++ ApplicationDbContext.cs
@@ -8,6 +8,7 @@ namespace CommunalServices
         public DbSet<Abonent> Abonent { get; set; }
         public DbSet<Flat> Flat { get; set; }
         public DbSet<Debt> Debt { get; set; }
+        public DbSet<PaymentAccount> PaymentAccount { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
     }
 }
