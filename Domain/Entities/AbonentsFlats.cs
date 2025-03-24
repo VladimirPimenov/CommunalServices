7ecee4e88a6b0357f02bdd0da38c6d9eba270da3
Vladimir <pimenovv2000@gmail.com>
2025-03-24 23:33:09 +0300

    Добавлена сущность для связи Abonent и Flat

---
+++ Domain/Entities/AbonentsFlats.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CommunalServices.Domain.Entities
+{
+    public class AbonentsFlats
+    {
+        [Key]
+        public int OwnerDocumentId { get; set; }
+        public int AbonentId { get; set; }
+        public string PaymentNumber { get; set; }
+    }
+}
