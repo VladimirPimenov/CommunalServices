532f1078dd82af5a8b8334860576dec75ce10e97
Vladimir <pimenovv2000@gmail.com>
2025-03-25 00:55:40 +0300

    Добавлена логика входа и изменения пароля

---
+++ Domain/AbonentLogger.cs
@@ -0,0 +1,39 @@
+﻿using CommunalServices.Domain.Entities;
+using CommunalServices.Domain.DTO;
+
+namespace CommunalServices.Domain
+{
+    public class AbonentLogger(IRepository _repository):IAbonentLogger
+    {
+        public async Task<AbonentDTO> TryLoginAsync(string login, string password)
+        {
+            var abonent = await _repository.GetAbonentByLoginAsync(login);
+
+            if (abonent == null || !abonent.IsValidPassword(password)) return null;
+
+            return AbonentDTO.FromAbonent(abonent);
+        }
+
+        public async Task<AbonentDTO> ChangePassword(AbonentDTO updatedAbonent)
+        {
+            var abonent = await _repository.GetAbonentByLoginAsync(updatedAbonent.Login);
+
+            abonent.Password = updatedAbonent.Password;
+
+            await _repository.UpdateAbonentAsync(abonent);
+
+            return AbonentDTO.FromAbonent(abonent);
+        }
+
+        public async Task<List<Flat>> GetAbonentFlats(string login)
+        {
+            var abonent = await _repository.GetAbonentByLoginAsync(login);
+
+            if (abonent == null) return null;
+
+            var flats = await _repository.GetAbonentFlatsAsync(abonent);
+
+            return flats;
+        }
+    }
+}
---
+++ Domain/IAbonentLogger.cs
@@ -0,0 +1,14 @@
+﻿using CommunalServices.Domain.DTO;
+using CommunalServices.Domain.Entities;
+
+namespace CommunalServices.Domain
+{
+    public interface IAbonentLogger
+    {
+        public Task<AbonentDTO> TryLoginAsync(string login, string password);
+
+        public Task<AbonentDTO> ChangePassword(AbonentDTO updatedAbonent);
+
+        public Task<List<Flat>> GetAbonentFlats(string login);
+    }
+}
