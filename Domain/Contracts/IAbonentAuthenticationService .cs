8e87826c79c5b249e8c5a7d9c0c62378d676228f
Vladimir <pimenovv2000@gmail.com>
2025-03-27 00:18:19 +0300

    Разделение на контракты и их реализацию

---
+++ Domain/Contracts/IAbonentAuthenticationService .cs
@@ -0,0 +1,12 @@
+﻿using CommunalServices.Domain.DTO;
+using CommunalServices.Domain.Entities;
+
+namespace CommunalServices.Domain.Contracts
+{
+    public interface IAbonentAuthenticationService
+    {
+        public Task<Abonent> TryRegisterAsync(AbonentDTO newAbonent);
+        public Task<Abonent> TryLoginAsync(string login, string password);
+        public Task<Abonent> ChangePasswordAsync(AbonentDTO updatedAbonent);
+    }
+}
---
+++ Domain/ContractsRealization/AbonentAuthenticationService .cs
@@ -0,0 +1,35 @@
+﻿using CommunalServices.Domain.Entities;
+using CommunalServices.Domain.DTO;
+using CommunalServices.Domain.Contracts;
+
+namespace CommunalServices.Domain.ContractsRealization
+{
+    public class AbonentAuthenticationService(IRepository _repository) : IAbonentAuthenticationService
+    {
+        public async Task<Abonent> TryRegisterAsync(AbonentDTO newAbonent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Abonent> TryLoginAsync(string login, string password)
+        {
+            var abonent = await _repository.GetAbonentByLoginAsync(login);
+
+            if (abonent == null || !abonent.IsValidPassword(password))
+                return null;
+
+            return abonent;
+        }
+
+        public async Task<Abonent> ChangePasswordAsync(AbonentDTO updatedAbonent)
+        {
+            var abonent = await _repository.GetAbonentByLoginAsync(updatedAbonent.Login);
+
+            abonent.Password = updatedAbonent.Password;
+
+            await _repository.UpdateAbonentAsync(abonent);
+
+            return abonent;
+        }
+    }
+}
