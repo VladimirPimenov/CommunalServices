fc20d2a7f7d4c774757069bbb037090d5556abf3
Vladimir <pimenovv2000@gmail.com>
2025-04-01 00:06:16 +0300

    Документирование

--- Domain/Contracts/IDebtPaymentService.cs
+++ Domain/Contracts/IDebtPaymentService.cs
@@ -2,8 +2,17 @@
 
 namespace CommunalServices.Domain.Contracts
 {
+    /// <summary>
+    /// Интерфейс для сервиса осуществления оплаты задолженностей.
+    /// Этот интерфейс определяет методы для обработки платежей по задолженностям.
+    /// </summary>
     public interface IDebtPaymentService
     {
+        /// <summary>
+        /// Обрабатывает оплату задолженности.
+        /// </summary>
+        /// <param name="debtId">Идентификатор задолженности, которую нужно оплатить.</param>
+        /// <returns>Возвращает объект задолженности после успешной оплаты; иначе null.</returns>
         public Task<Debt> PayDebtAsync(int debtId);
     }
 }
