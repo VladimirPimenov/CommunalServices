ce08bea9fb311a3ed93355b6e413f3a3f5db1a35
Vladimir <pimenovv2000@gmail.com>
2025-04-10 01:33:37 +0300

    Добавлены контроллеры для авторизации и оплаты

---
+++ Controllers/AuthentificationController.cs
@@ -0,0 +1,47 @@
+﻿using CommunalServices.Domain.Contracts;
+using CommunalServices.Domain.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommunalServices.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AuthentificationController(IAbonentAuthenticationService _abonentAuthService): ControllerBase
+    {
+        [Route("Login")]
+        [HttpGet]
+        public async Task<IActionResult> LoginAsync(string login, string password)
+        {
+            var abonent = await _abonentAuthService.LoginAsync(login, password);
+
+            if (abonent == null)
+                return NotFound();
+
+            return Ok(abonent);
+        }
+
+        [Route("Register")]
+        [HttpPost]
+        public async Task<IActionResult> RegisterAsync(AbonentDTO newAbonent)
+        {
+            var registeredAbonent = await _abonentAuthService.RegisterAsync(newAbonent);
+
+            if (registeredAbonent == null)
+                return BadRequest();
+
+            return Ok(registeredAbonent);
+        }
+
+        [Route("ChangePassword")]
+        [HttpPut]
+        public async Task<IActionResult> ChangePassword(AbonentDTO updatedAbonent)
+        {
+            var abonent = await _abonentAuthService.ChangePasswordAsync(updatedAbonent);
+
+            if (abonent == null)
+                return NotFound();
+
+            return Ok(abonent);
+        }
+    }
+}
---
+++ Controllers/DebtPaymentController.cs
@@ -0,0 +1,35 @@
+﻿using CommunalServices.Domain.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommunalServices.Controllers
+{
+    [Route("Payment")]
+    [ApiController]
+    public class DebtPaymentController(
+        IDebtPaymentService _debtPayService, 
+        IFlatDebtsQueryService _flatDebtsQueryService) : ControllerBase
+    {
+        [Route("GetDebts")]
+        [HttpGet]
+        public async Task<IActionResult> GetFlatDebts(string paymentNumber)
+        {
+            var debts = await _flatDebtsQueryService.GetFlatDebtsAsync(paymentNumber);
+
+            if (debts == null)
+                return NotFound();
+            return Ok(debts);
+        }
+
+        [Route("PayDebt")]
+        [HttpDelete]
+        public async Task<IActionResult> RemoveDebt(int debtId)
+        {
+            var paidDebt = await _debtPayService.PayDebtAsync(debtId);
+
+            if (paidDebt == null)
+                return NotFound();
+
+            return Ok(paidDebt);
+        }
+    }
+}
