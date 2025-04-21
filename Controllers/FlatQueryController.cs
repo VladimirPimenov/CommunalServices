9300f9018bd6b7a8ea5e61b2d09e7e9913a5143e
Vladimir Pimenov <167480900+VladimirPimenov@users.noreply.github.com>
2025-04-22 00:27:52 +0300

    Добавлен контроллер для получения квартир абонента

---
+++ Controllers/FlatQueryController.cs
@@ -0,0 +1,17 @@
+﻿using CommunalServices.Domain.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommunalServices.Controllers
+{
+    [Route("Flats")]
+    [ApiController]
+    public class FlatQueryController(IAbonentFlatsQueryService _abonentFlatsQueryService) : ControllerBase
+    {
+        [HttpGet("GetAbonentFlats")]
+        public async Task<IActionResult> GetAbonentFlats(string abonentLogin)
+        {
+            var flats = _abonentFlatsQueryService.GetAbonentFlatsAsync(abonentLogin);
+            return flats == null ? NotFound() : Ok(flats);
+        }
+    }
+}
