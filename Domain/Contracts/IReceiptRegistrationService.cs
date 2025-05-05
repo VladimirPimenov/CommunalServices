43c880a8ee332f19e71c2e12b196bd7f01366b5f
Vladimir Pimenov <167480900+VladimirPimenov@users.noreply.github.com>
2025-05-06 01:44:18 +0300

    Добавлен сервис регистрации электронных чеков через API Атол

---
+++ Domain/Contracts/IReceiptRegistrationService.cs
@@ -0,0 +1,18 @@
+﻿using CommunalServices.Domain.Entities;
+
+namespace CommunalServices.Domain.Contracts
+{
+    /// <summary>
+    /// Интерфейс сервиса регистрации чеков
+    /// </summary>
+    public interface IReceiptRegistrationService
+    {
+        /// <summary>
+        /// Регистрирует чек для указанного абонента и счета оплаты
+        /// </summary>
+        /// <param name="abonentId">Идентификатор абонента</param>
+        /// <param name="paymentAccountId">Идентификатор счета оплаты</param>
+        /// <returns>true, если чек успешно зарегистрирован; иначе false</returns>
+        public Task<bool> RegisterReceiptAsync(int abonentId, int paymentAccountId);
+    }
+}
---
+++ Domain/UseCase/AtolReceiptRegistrationService.cs
@@ -0,0 +1,41 @@
+﻿using CommunalServices.Domain.Contracts;
+using CommunalServices.Domain.Entities;
+using System.Text.Json;
+
+namespace CommunalServices.Domain.UseCase
+{
+    public class AtolReceiptRegistrationService(IRepository _repository) : IReceiptRegistrationService
+    {
+        public async Task<bool> RegisterReceiptAsync(int abonentId, int paymentAccountId)
+        {
+            var abonent = await _repository.GetAbonentByIdAsync(abonentId);
+            var paymentAccount = await _repository.GetPaymentAccountByIdAsync(paymentAccountId);
+
+            if (abonent == null || paymentAccount == null)
+                return false;
+
+            var json = CreateJsonReceipt(abonent, paymentAccount);
+
+            bool receiptResult = await SendJsonReceiptToAtol(json);
+            return receiptResult;
+        }
+
+        private JsonContent CreateJsonReceipt(Abonent abonent, PaymentAccount paymentAccount)
+        {
+            var mockRequest = new { abonent, paymentAccount };
+
+            var json = JsonContent.Create(mockRequest);
+
+            return json;
+        }
+
+        private async Task<bool> SendJsonReceiptToAtol(JsonContent receipt)
+        {
+            HttpClient httpClient = new HttpClient();
+
+            var httpResponce = await httpClient.PostAsync("https://online.atol.ru/possystem/v5/sell", receipt);
+
+            return httpResponce.IsSuccessStatusCode; 
+        }
+    }
+}
